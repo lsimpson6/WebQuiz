d15f442e1962128540085d0146f2651ae8237a53
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-05-30 13:58:56 -0400

    start file

--- WebQuiz.js
+++ WebQuiz.js
@@ -1,1 +1,31 @@
+var question = document.getAttribute('data-question');
+var selections = document.querySelectorAll('.selections');
+var result = document.querySelectorAll('.result');
 
+const correct = "Correct!", wrong = "Incorrect!";
+
+var correctAnswers = {
+    "1": {q: 1, a: "This is answer 1", d: ""},
+    "2": {q: 2, a: "This is answer 4", d: ""},
+    "3": {q: 3, a: "This is answer 2", d: ""},
+    "4": {q: 4, a: "This is answer 3", d: ""}
+}
+
+selections.forEach('click', s => {
+
+    var questionNum = s.getAttribute('data-selection');
+
+    for(let i = 0; i < correctAnswers.length; i++){
+        if(i = questionNum){
+
+            var answer = correctAnswers[questionNum].a;
+            var desc = correctAnswers[questionNum].d;
+
+            result[questionNum].classList.remove('d-none');
+
+            alert('correct');
+
+        }
+    }
+
+})
