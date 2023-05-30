a55abee85d9b31e61524c79c82a621430467ec94
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-05-30 14:34:39 -0400

    checking for proper values - 1

--- WebQuiz.js
+++ WebQuiz.js
@@ -4,25 +4,27 @@ var result = document.querySelectorAll('.result');
 const correct = "Correct!", wrong = "Incorrect!";
 
 var correctAnswers = {
-    "1": {q: 1, a: 2, d: ""},
-    "2": {q: 2, a: 3, d: ""},
-    "3": {q: 3, a: 1, d: ""},
-    "4": {q: 4, a: 3, d: ""}
+    "1": {answer: 2, d: ""},
+    "2": {answer: 3, d: ""},
+    "3": {answer: 1, d: ""},
+    "4": {answer: 3, d: ""}
 }
+
 console.log("loaded, you may now click");
+
 selection.forEach(s => s.addEventListener('click', ()=>{
-    console.log("clicked - start");
+
     var selectedAnswerNumber = s.getAttribute('data-selection');
     var questionNum = s.getAttribute('data-question');
 
-        if(correctAnswers[questionNum].a = selectedAnswerNumber){
+        if(correctAnswers[questionNum].answer == selectedAnswerNumber){
 
             var answer = correctAnswers[questionNum].a;
             var desc = correctAnswers[questionNum].d;
 
             result[questionNum - 1].classList.remove('d-none');
 
-            console.log("qestion: " + questionNum + " correct ans: " + correctAnswers[questionNum].a +  " selected ans: " + selectedAnswerNumber);
+            console.log("qestion: " + questionNum + " correct ans: " + correctAnswers[questionNum].answer +  " selected ans: " + selectedAnswerNumber);
             console.log("correct");
 
         }
