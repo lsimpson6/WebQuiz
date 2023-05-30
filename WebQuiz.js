c9c5e82b375f8131bd0fa17e909d8f36d40d3bd0
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-05-30 14:28:16 -0400

    bug fixes

--- WebQuiz.js
+++ WebQuiz.js
@@ -1,22 +1,21 @@
-var question = document.querySelectorAll('.questions');
 var selection = document.querySelectorAll('.selections');
 var result = document.querySelectorAll('.result');
 
 const correct = "Correct!", wrong = "Incorrect!";
 
 var correctAnswers = {
-    "1": {q: 1, a: "This is answer 1", d: ""},
-    "2": {q: 2, a: "This is answer 4", d: ""},
-    "3": {q: 3, a: "This is answer 2", d: ""},
-    "4": {q: 4, a: "This is answer 3", d: ""}
+    "1": {q: 1, a: 2, d: ""},
+    "2": {q: 2, a: 3, d: ""},
+    "3": {q: 3, a: 1, d: ""},
+    "4": {q: 4, a: 3, d: ""}
 }
 console.log("loaded, you may now click");
 selection.forEach(s => s.addEventListener('click', ()=>{
     console.log("clicked - start");
-    var questionNum = s.getAttribute('data-selection');
+    var selectedAnswerNumber = s.getAttribute('data-selection');
+    var questionNum = s.getAttribute('data-question');
 
-    for(let i = 0; i < 4; i++){
-        if(i = questionNum){
+        if(correctAnswers[questionNum].a = selectedAnswerNumber){
 
             var answer = correctAnswers[questionNum].a;
             var desc = correctAnswers[questionNum].d;
@@ -24,8 +23,11 @@ selection.forEach(s => s.addEventListener('click', ()=>{
             result[questionNum - 1].classList.remove('d-none');
 
             console.log(questionNum);
-            console.log("within if - done");
+            console.log("correct");
 
         }
-    }
+        else{
+            console.log("incorrect or wrong");
+        }
+
 }))
